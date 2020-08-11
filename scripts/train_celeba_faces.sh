b8991e35ac4368108e5e9550c7e123b23b874c41
yhlleo <yahui.cvrs@gmail.com>
2020-08-11 13:43:39 +0800

    update

--- scripts/train_celeba_faces.sh
+++ scripts/train_celeba_faces.sh
@@ -1,6 +1,6 @@
 GPU_ID=$1
 RESUME=$2
 
-CUDA_VISIBLE_DEVICES=${GPU_ID} python gmm_train.py \
+CUDA_VISIBLE_DEVICES=${GPU_ID} python train.py \
   --config configs/celeba_faces.yaml \
   --resume ${RESUME} 
